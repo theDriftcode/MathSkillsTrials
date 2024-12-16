6e87bfaae89b360b18bec29468f72294581ff1a5
Gabriel Garcia <496driftyhealer@gmail.com>
2024-12-16 15:40:39 -0800

    The start of a brand new project

---
+++ main.py
@@ -0,0 +1,39 @@
+##################################################################################################################################
+#
+#   Welcome to my MathSkillSet Python game, where you can trail yourself to be quicker with math problems at the simplest levels. 
+#    
+#   @author: Gabriel Garcia (theGcode)
+#   @version 1.0
+#   @date 2024-12-16
+#
+#   @Description
+#   This is a simple math game uses tkinter to display the problems where the user is presented with a series of math problems.
+#   The user is asked to solve the problem and the game keeps track of the user's score
+#   The user can choose between different modes:
+#   
+#   Mode_01(Time_Deathrun):    
+#       The User will get prompted with various math questions which they must correctly answer as quickly as possible. 
+#       Each correct answer will increase the score by 1 point.
+#       The game ends if the user answers 3 problems incorrectly (time ran out or incorrect answer) or reaches the end of the trial.
+#
+#   Mode_02(SpeedRun):
+#       The User will get prompted with various math questions which they must correctly answer as quickly as possible
+#       Each correct answer will increase the score by 1 point.
+#       The game ends if the user answers 3 problems incorrectly
+#       At the end the game will display the users final score
+#       The user can skip however if they do, they automatically loose 2 hearts. Only have onw chance to get the rest of the answers correctly. 
+#
+#   Mode_03(BuildUP):
+#       The User will get prompted with various math questions which they must correctly answer as quickly as possible
+#       The game increase in difficulty as they progress more and more.
+#       Each correct answer will increase the score by 1 point.
+#       The game ends if the user answers 3 problems incorrectly
+#       The user can skip however if they do, they automatically loose 1 heart
+#       After a few questions right the user can regain a heart back. 
+
+#
+#
+#
+#
+#
+#########################################################################################################################################
